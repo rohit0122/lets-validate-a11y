f9bcfcc7de4d2f452fda2e28a5bfea2af6b212d8
Rohit Shrivastava <rohit.shrivastava@tadigital.com>
2023-05-04 17:47:06 +0530

    Updated config for sample

--- config.js
+++ config.js
@@ -3,15 +3,15 @@
  * If you want to run in bulk, if `findHtmlFromHere` is set, config will run on that path.
  * Set to FALSE if you want to run for selected files.
  */
-export const findHtmlFromHere =  '/Users/rohit.shrivastava/Documents/Projects/AARP/Repos/uxdia/ui.apps/src/main/content/jcr_root/apps/uxdia/';
+export const findHtmlFromHere =  '/users/rohit.shrivastava/documents/projects/my-codebase/';
 
 
 /**
  * Varible `findHtmlFromHere` has priority, if you want to use script for selected file, set `findHtmlFromHere` to false.
  */
 export const filesToVerify = [
-    '/Users/rohit.shrivastava/Documents/Projects/AARP/Repos/uxdia/ui.apps/src/main/content/jcr_root/apps/uxdia/components/content/quizcontentfragment/quizcontentfragment.html',
-    '/Users/rohit.shrivastava/Documents/Projects/AARP/Repos/uxdia/ui.apps/src/main/content/jcr_root/apps/uxdia/components/content/quizcontentfragment/element.html'
+    '/users/rohit.shrivastava/documents/projects/my-codebase/quizcontentfragment/quizcontentfragment.html',
+    '/users/rohit.shrivastava/documents/projects/my-codebase/quizcontentfragment/element.html'
 ];
 
 /**
